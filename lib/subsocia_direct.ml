4ba6bccb0667dc025ca63c6c9a98177b496e253a
Petter Urkedal <paurkedal@gmail.com>
2014-10-14 08:43:08 +0200

    Drop AMENDED_CONNECTION.

--- lib/subsocia_direct.ml
+++ lib/subsocia_direct.ml
@@ -192,22 +192,6 @@ module Base = struct
 
 end
 
-module type AMENDED_CONNECTION = sig
-  open Base
-  include CONNECTION
-
-  val fetch_entity_type : entity_type_id -> entity_type Lwt.t
-  val decode : tuple -> entity
-  val fetch : entity_id -> entity Lwt.t
-  val min_max : int -> entity list Lwt.t
-  val pred_succ : int -> int32 -> entity list Lwt.t
-  val precedes : int32 -> int32 -> bool Lwt.t
-  val create : entity_type: entity_type -> viewer: entity -> admin: entity ->
-	       unit -> entity Lwt.t
-  val constrain : entity -> entity -> unit Lwt.t
-  val unconstrain : entity -> entity -> unit Lwt.t
-end
-
 let connect uri = (module struct
 
   include Base
@@ -235,10 +219,20 @@ let connect uri = (module struct
 
   let inclusion_cache = Prime_cache.create ~cache_metric 61
 
-  module Amend_connection (C : CONNECTION) = struct
-    include C
+  let pool =
+    let connect () = Caqti_lwt.connect uri in
+    let disconnect (module C : CONNECTION) = C.disconnect () in
+    let validate (module C : CONNECTION) = C.validate () in
+    let check (module C : CONNECTION) = C.check in
+    Caqti_lwt.Pool.create ~validate ~check connect disconnect
 
-    let fetch_entity_type et_id =
+  let use f = Caqti_lwt.Pool.use f pool
+
+  module Entity_type = struct
+    type id = int32
+    type t = entity_type
+
+    let fetch' et_id (module C : CONNECTION) =
       let id_p = C.Param.([|int32 et_id|]) in
       match_lwt C.find Q.fetch_entity_type
 		       C.Tuple.(fun tup -> text 1 tup, text 2 tup) id_p with
@@ -270,73 +264,12 @@ let connect uri = (module struct
 	    et_name; et_display_name; et_display_name_pl;
 	    et_preds; et_succs; et_beacon }
 
-    let decode tup =
-      Entity_by_id.merge entity_by_id @@
-      Beacon.embed entity_grade @@ fun e_beacon ->
-      { e_id = Tuple.int32 0 tup;
-	e_type_id = Tuple.int32 1 tup;
-	e_rank = Tuple.int 2 tup;
-	e_viewer_id = Tuple.int32 3 tup;
-	e_admin_id = Tuple.int32 4 tup;
-	e_pred_succ = Weak.create 2;
-	e_beacon; }
-
-    let fetch id =
-      match_lwt C.find Q.fetch decode C.Param.([|int32 id|]) with
-      | None -> Lwt.fail Not_found
-      | Some e -> Lwt.return e
-    let fetch_viewer e = fetch e.e_viewer_id
-    let fetch_admin e = fetch e.e_admin_id
-
-    let min_max i = C.fold Q.select_min_max.(i) (List.push *< decode) [||] []
-    let pred_succ i entity_id =
-      C.fold Q.select_pred_succ.(i) (List.push *< decode)
-	     Param.([|int32 entity_id|]) []
-
-    let precedes subentity_id superentity_id =
-      C.find Q.select_precedes (fun _ -> ())
-	     Param.([|int32 subentity_id; int32 superentity_id|]) >|=
-      function None -> false | Some () -> true
-
-    let create ~entity_type ~viewer ~admin () =
-      lwt entity_id_opt =
-	C.find Q.create_entity C.Tuple.(int32 0)
-	  C.Param.([|int32 entity_type.et_id;
-		     int32 viewer.e_id; int32 admin.e_id|]) in
-      fetch (Option.get entity_id_opt)
-
-    let constrain subentity superentity =
-      C.exec Q.insert_inclusion
-	C.Param.([|int32 subentity.e_id; int32 superentity.e_id|])
-
-    let unconstrain subentity superentity =
-      C.exec Q.delete_inclusion
-	C.Param.([|int32 subentity.e_id; int32 superentity.e_id|])
-  end
-
-  let pool =
-    let connect () =
-      lwt c = Caqti_lwt.connect uri in
-      let module C = (val c) in
-      let module C' = Amend_connection (C) in
-      Lwt.return (module C' : AMENDED_CONNECTION) in
-    let disconnect (module C : AMENDED_CONNECTION) = C.disconnect () in
-    let validate (module C : AMENDED_CONNECTION) = C.validate () in
-    let check (module C : AMENDED_CONNECTION) = C.check in
-    Caqti_lwt.Pool.create ~validate ~check connect disconnect
-
-  let use f = Caqti_lwt.Pool.use f pool
-
-  module Entity_type = struct
-    type id = int32
-    type t = entity_type
-
     let fetch et_id =
       try
 	let et = {dummy_entity_type with et_id} in
 	Lwt.return (Entity_type_by_id.find entity_type_by_id et)
       with Not_found ->
-	use (fun (module C) -> C.fetch_entity_type et_id)
+	use (fetch' et_id)
 
     let get_id {et_id} = et_id
     let get_name {et_name} = et_name
@@ -369,13 +302,30 @@ let connect uri = (module struct
     let get_viewer_id {e_viewer_id} = e_viewer_id
     let get_admin_id {e_admin_id} = e_admin_id
 
+    let decode (type tuple) (module C : CONNECTION with type Tuple.t = tuple)
+	       (tup : tuple) =
+      Entity_by_id.merge entity_by_id @@
+      Beacon.embed entity_grade @@ fun e_beacon ->
+      { e_id = C.Tuple.int32 0 tup;
+	e_type_id = C.Tuple.int32 1 tup;
+	e_rank = C.Tuple.int 2 tup;
+	e_viewer_id = C.Tuple.int32 3 tup;
+	e_admin_id = C.Tuple.int32 4 tup;
+	e_pred_succ = Weak.create 2;
+	e_beacon; }
+
     let fetch e_id =
       try Lwt.return (Entity_by_id.find entity_by_id {dummy_entity with e_id})
-      with Not_found -> use (fun (module C) -> C.fetch e_id)
+      with Not_found ->
+	use @@ fun (module C) ->
+	match_lwt C.find Q.fetch (decode (module C))
+			 C.Param.([|int32 e_id|]) with
+	| None -> Lwt.fail Not_found
+	| Some e -> Lwt.return e
 
-    let fetch_type {e_type_id} = Entity_type.fetch e_type_id
-    let fetch_admin {e_admin_id} = fetch e_admin_id
-    let fetch_viewer {e_viewer_id} = fetch e_viewer_id
+    let fetch_type e = Entity_type.fetch e.e_type_id
+    let fetch_viewer e = fetch e.e_viewer_id
+    let fetch_admin e = fetch e.e_admin_id
 
     let min_max_cache = Weak.create 2
 
@@ -383,7 +333,9 @@ let connect uri = (module struct
       match Weak.get min_max_cache i with
       | Some el -> Beacon.charge el.el_beacon; Lwt.return el.el_entities
       | None ->
-	lwt el_entities = use (fun (module C) -> C.min_max i) in
+	lwt el_entities = use @@ fun (module C) ->
+	  C.fold Q.select_min_max.(i)
+		 (List.push *< decode (module C)) [||] [] in
 	begin match Weak.get min_max_cache i with
 	| Some el -> Beacon.charge el.el_beacon; Lwt.return el.el_entities
 	| None ->
@@ -399,7 +351,9 @@ let connect uri = (module struct
       match Weak.get e.e_pred_succ i with
       | Some el -> Beacon.charge el.el_beacon; Lwt.return el.el_entities
       | None ->
-	lwt el_entities = use (fun (module C) -> C.pred_succ i e.e_id) in
+	lwt el_entities = use @@ fun (module C) ->
+	  C.fold Q.select_pred_succ.(i) (List.push *< decode (module C))
+		 C.Param.([|int32 e.e_id|]) [] in
 	begin match Weak.get e.e_pred_succ i with
 	| Some el -> Beacon.charge el.el_beacon; Lwt.return el.el_entities
 	| None ->
@@ -412,17 +366,32 @@ let connect uri = (module struct
     let fetch_succs = fetch_pred_succ 1
 
     let create ~entity_type ~viewer ~admin () =
-      use (fun (module C) -> C.create ~entity_type ~viewer ~admin ())
+      use @@ fun (module C) ->
+	lwt entity_id_opt =
+	  C.find Q.create_entity C.Tuple.(int32 0)
+	    C.Param.([|int32 entity_type.et_id;
+		       int32 viewer.e_id; int32 admin.e_id|]) in
+	fetch (Option.get entity_id_opt)
 
     let check_preceq subentity superentity =
       if subentity.e_id = superentity.e_id then Lwt.return_true else
       let k = subentity.e_id, superentity.e_id in
       try Lwt.return (Prime_cache.find inclusion_cache k)
       with Not_found ->
-	lwt c = use (fun (module C) ->
-		     C.precedes subentity.e_id superentity.e_id) in
+	lwt c = use @@ fun (module C) ->
+	  C.find Q.select_precedes (fun _ -> ())
+		 C.Param.([|int32 subentity.e_id; int32 superentity.e_id|]) >|=
+	  function None -> false | Some () -> true in
 	Prime_cache.replace inclusion_cache preceq_grade k c;
 	Lwt.return c
+
+    let constrain' subentity superentity (module C : CONNECTION) =
+      C.exec Q.insert_inclusion
+	C.Param.([|int32 subentity.e_id; int32 superentity.e_id|])
+
+    let unconstrain' subentity superentity (module C : CONNECTION) =
+      C.exec Q.delete_inclusion
+	C.Param.([|int32 subentity.e_id; int32 superentity.e_id|])
 
     let constrain subentity superentity =
       lwt is_sub = check_preceq subentity superentity in
@@ -430,14 +399,14 @@ let connect uri = (module struct
       lwt is_super = check_preceq superentity subentity in
       if is_super then Lwt.fail (Invalid_argument "cyclic constraint") else
       (* TODO: Update entity_rank. *)
-      use (fun (module C) -> C.constrain subentity superentity)
+      use (constrain' subentity superentity)
       (* TODO: Update is_subsumed. *)
       (* FIXME: Clear or update pred and succ caches. *)
       (* FIXME: Clear or update inclusion cache. *)
 
     let unconstrain subentity superentity =
       (* TODO: Update is_subsumed. *)
-      use (fun (module C) -> C.unconstrain subentity superentity)
+      use (unconstrain' subentity superentity)
       (* TODO: Update entity_rank. *)
       (* FIXME: Clear or update pred and succ caches. *)
       (* FIXME: Clear or update inclusion cache. *)
