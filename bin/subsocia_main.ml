56792b26e395b948889c7708cdb1aced60f27724
Petter Urkedal <paurkedal@gmail.com>
2015-02-23 23:42:54 +0100

    Add it-list subcommand.

--- bin/subsocia_main.ml
+++ bin/subsocia_main.ml
@@ -24,10 +24,11 @@ let connect () =
   Subsocia_direct.connect uri
 
 let run f = Lwt_main.run (f (connect ()))
+let run0 f = Lwt_main.run (f (connect ())); 0
 
 (* Entity Types *)
 
-let et_create etn = run @@ fun (module C) ->
+let et_create etn = run0 @@ fun (module C) ->
   lwt et = C.Entity_type.create etn in
   Lwt_log.info_f "Created type #%ld = %s." (C.Entity_type.id et) etn
 
@@ -45,17 +46,17 @@ let et_delete etn = run @@ fun (module C) ->
     let et_id = C.Entity_type.id et in
     C.Entity_type.delete et >>
     Lwt_log.info_f "Deleted type #%ld = %s." et_id etn >>
-    Lwt.return (`Ok ())
+    Lwt.return (`Ok 0)
 
 let et_delete_t =
   let et_name_t =
     Arg.(required & pos 0 (some string) None & info ~docv:"TYPE-NAME" []) in
   Term.(ret (pure et_delete $ et_name_t))
 
-let et_list () = run @@ fun (module C) ->
+let et_list () = run0 @@ fun (module C) ->
   C.Entity_type.all () >>=
   C.Entity_type.Set.iter_s
-    (fun et -> C.Entity_type.name et >>= Lwt_io.printf "%s\n")
+    (fun et -> C.Entity_type.name et >>= Lwt_io.printl)
 
 let et_list_t = Term.(pure et_list $ pure ())
 
@@ -70,7 +71,7 @@ let it_allow etn0 etn1 = run @@ fun (module C) ->
   | Some et0, Some et1 ->
     let mu0, mu1 = Multiplicity.(May, May) in (* TODO *)
     C.Entity_type.inclusion_allow mu0 mu1 et0 et1 >>
-    Lwt.return (`Ok ())
+    Lwt.return (`Ok 0)
   | None, Some _ -> report_missing etn0
   | Some _, None -> report_missing etn1
   | None, None -> report_missing (etn0 ^ " and " ^ etn1)
@@ -82,7 +83,7 @@ let it_disallow etn0 etn1 = run @@ fun (module C) ->
     Lwt.return (`Error (false, "Missing types " ^ etns ^ ".")) in
   match et0, et1 with
   | Some et0, Some et1 -> C.Entity_type.inclusion_disallow et0 et1 >>
-			  Lwt.return (`Ok ())
+			  Lwt.return (`Ok 0)
   | None, Some _ -> report_missing etn0
   | Some _, None -> report_missing etn1
   | None, None -> report_missing (etn0 ^ " and " ^ etn1)
@@ -101,6 +102,44 @@ let it_disallow_t =
 		    info ~docv:"SUPER-TYPE" []) in
   Term.(ret (pure it_disallow $ etn0_t $ etn1_t))
 
+let it_list etn0_opt etn1_opt = run @@ fun (module C) ->
+  let get_et = function
+    | None | Some "_" -> Lwt.return_none
+    | Some etn ->
+      begin match_lwt C.Entity_type.of_name etn with
+      | None -> Lwt.fail (Failure ("No entity type is named " ^ etn ^ "."))
+      | Some et -> Lwt.return (Some et)
+      end in
+  lwt et0 = get_et etn0_opt in
+  lwt et1 = get_et etn1_opt in
+  let show_map = C.Entity_type.Map.iter_s @@ fun et (mu0, mu1) ->
+    lwt etn = C.Entity_type.name et in
+    Lwt_io.printlf "%s%s %s" (Multiplicity.to_string mu0)
+			     (Multiplicity.to_string mu1) etn in
+  match et0, et1 with
+  | None, None ->
+    Lwt.fail (Failure ("At least one entity type must be provided."))
+  | Some et0, None ->
+    C.Entity_type.inclusion_succs et0 >>= show_map >> Lwt.return 0
+  | None, Some et1 ->
+    C.Entity_type.inclusion_preds et1 >>= show_map >> Lwt.return 0
+  | Some et0, Some et1 ->
+    begin match_lwt C.Entity_type.inclusion et0 et1 with
+    | Some (mu0, mu1) ->
+      Lwt_io.printlf "%s%s" (Multiplicity.to_string mu0)
+			    (Multiplicity.to_string mu1) >>
+      Lwt.return 0
+    | None ->
+      Lwt.return 1
+    end
+
+let it_list_t =
+  let etn0_t = Arg.(value & pos 0 (some string) None &
+		    info ~docv:"SUB-TYPE" []) in
+  let etn1_t = Arg.(value & pos 1 (some string) None &
+		    info ~docv:"SUPER-TYPE" []) in
+  Term.(pure it_list $ etn0_t $ etn1_t)
+
 let subcommands = [
   et_list_t, Term.info ~doc:"List entity types." "et-list";
   et_create_t, Term.info ~doc:"Create an entity type." "et-create";
@@ -109,6 +148,7 @@ let subcommands = [
 			"it-allow";
   it_disallow_t, Term.info ~doc:"Disallow inclusion between entities of a type."
 			   "it-disallow";
+  it_list_t, Term.info ~doc:"Show inclusion policy between types." "it-list";
 ]
 
 let main_t = Term.(ret @@ pure (`Error (true, "Missing subcommand.")))
@@ -118,4 +158,5 @@ let () =
   | `Error `Parse -> exit 64
   | `Error `Term -> exit 69
   | `Error `Exn -> exit 70
-  | `Ok () | `Version | `Help -> exit 0
+  | `Ok rc -> exit rc
+  | `Version | `Help -> exit 0
