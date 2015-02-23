ce642965481f75b12dfdca38f5e9e096d44762cb
Petter Urkedal <paurkedal@gmail.com>
2015-02-24 00:51:14 +0100

    Implement command-line attribute management.

--- bin/subsocia_main.ml
+++ bin/subsocia_main.ml
@@ -26,6 +26,19 @@ let connect () =
 let run f = Lwt_main.run (f (connect ()))
 let run0 f = Lwt_main.run (f (connect ())); 0
 
+let value_type_parser s =
+  try `Ok (Type.of_string s) with Invalid_argument msg -> `Error msg
+let value_type_printer ftr vt =
+  Format.pp_print_string ftr (Type.string_of_t0 vt)
+let value_type_conv = value_type_parser, value_type_printer
+
+let multiplicity_parser s =
+  try `Ok (Multiplicity.of_string s)
+  with Invalid_argument msg -> `Error msg
+let multiplicity_printer ftr mu =
+  Format.pp_print_string ftr (Multiplicity.to_string mu)
+let multiplicity_conv = multiplicity_parser, multiplicity_printer
+
 (* Entity Types *)
 
 let et_create etn = run0 @@ fun (module C) ->
@@ -140,6 +153,73 @@ let it_list_t =
 		    info ~docv:"SUPER-TYPE" []) in
   Term.(pure it_list $ etn0_t $ etn1_t)
 
+(* Attributes *)
+
+let at_create atn vt = run0 @@ fun (module C) ->
+  C.Attribute_type.create vt atn >>= fun at ->
+  Lwt_log.info_f "Created attribute type #%ld %s." (C.Attribute_type.id at) atn
+
+let at_create_t =
+  let atn_t = Arg.(required & pos 0 (some string) None &
+		   info ~docv:"NAME" []) in
+  let vt_t = Arg.(required & pos 1 (some value_type_conv) None &
+		  info ~docv:"TYPE" []) in
+  Term.(pure at_create $ atn_t $ vt_t)
+
+let at_delete atn = run @@ fun (module C) ->
+  match_lwt C.Attribute_type.of_name atn with
+  | Some at ->
+    C.Attribute_type.delete at >>
+    Lwt_log.info_f "Delete attribute type #%ld %s."
+		   (C.Attribute_type.id at) atn >>
+    Lwt.return 0
+  | None ->
+    Lwt_log.error_f "No attribute type is named %s." atn >>
+    Lwt.return 1
+
+let at_delete_t =
+  let atn_t = Arg.(required & pos 0 (some string) None &
+		   info ~docv:"NAME" []) in
+  Term.(pure at_delete $ atn_t)
+
+let req what name = function
+  | None -> Lwt.fail (Failure ("There is no " ^ what ^ " named " ^ name ^ "."))
+  | Some x -> Lwt.return x
+
+let at_allow atn etn0 etn1 mu = run0 @@ fun (module C) ->
+  lwt at = C.Attribute_type.of_name atn >>= req "attribute type" atn in
+  lwt et0 = C.Entity_type.of_name etn0 >>= req "entity type" etn0 in
+  lwt et1 = C.Entity_type.of_name etn1 >>= req "entity type" etn1 in
+  C.Entity_type.attribution_allow et0 et1 at mu
+
+let at_allow_t =
+  let atn_t = Arg.(required & pos 0 (some string) None &
+		   info ~docv:"NAME" []) in
+  let etn0_t = Arg.(required & pos 1 (some string) None &
+		    info ~docv:"SUB-TYPE" []) in
+  let etn1_t = Arg.(required & pos 2 (some string) None &
+		    info ~docv:"SUPER-TYPE" []) in
+  let mu = Arg.(value & pos 3 multiplicity_conv Multiplicity.May1 &
+		info ~docv:"MULTIPLICITY" []) in
+  Term.(pure at_allow $ atn_t $ etn0_t $ etn1_t $ mu)
+
+let at_disallow atn etn0 etn1 = run0 @@ fun (module C) ->
+  lwt at = C.Attribute_type.of_name atn >>= req "attribute type" atn in
+  lwt et0 = C.Entity_type.of_name etn0 >>= req "entity type" etn0 in
+  lwt et1 = C.Entity_type.of_name etn1 >>= req "entity type" etn1 in
+  C.Entity_type.attribution_disallow et0 et1 at
+
+let at_disallow_t =
+  let atn_t = Arg.(required & pos 0 (some string) None &
+		   info ~docv:"NAME" []) in
+  let etn0_t = Arg.(required & pos 1 (some string) None &
+		    info ~docv:"SUB-TYPE" []) in
+  let etn1_t = Arg.(required & pos 2 (some string) None &
+		    info ~docv:"SUPER-TYPE" []) in
+  Term.(pure at_disallow $ atn_t $ etn0_t $ etn1_t)
+
+(* Main *)
+
 let subcommands = [
   et_list_t, Term.info ~doc:"List entity types." "et-list";
   et_create_t, Term.info ~doc:"Create an entity type." "et-create";
@@ -149,6 +229,10 @@ let subcommands = [
   it_disallow_t, Term.info ~doc:"Disallow inclusion between entities of a type."
 			   "it-disallow";
   it_list_t, Term.info ~doc:"Show inclusion policy between types." "it-list";
+  at_create_t, Term.info ~doc:"Create an attribute type." "at-create";
+  at_delete_t, Term.info ~doc:"Delete an attribute type." "at-delete";
+  at_allow_t, Term.info ~doc:"Allow an attribution." "at-allow";
+  at_disallow_t, Term.info ~doc:"Disallow an attribution." "at-disallow";
 ]
 
 let main_t = Term.(ret @@ pure (`Error (true, "Missing subcommand.")))
