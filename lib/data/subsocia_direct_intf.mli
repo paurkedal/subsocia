3b40a241b48bab1f74085c14009feaa5f5c36de6
Petter A. Urkedal <paurkedal@gmail.com>
2016-08-14 09:40:36 +0200

    Simplify sharing constraint.

--- lib/data/subsocia_direct_intf.mli
+++ lib/data/subsocia_direct_intf.mli
@@ -20,17 +20,11 @@ module type S = sig
   val entity_changed : Entity.t -> [`Dsub | `Dsuper | `Asub | `Asuper] React.E.t
 
   module type T = Subsocia_intf.S
-    with type 'a Attribute_type.t = 'a Attribute_type.t
-     and type Attribute_type.ex = Attribute_type.ex
-     and type Attribute_type.Set.t = Attribute_type.Set.t
-     and type 'a Attribute_type.Map.t = 'a Attribute_type.Map.t
-     and type Relation.t = Relation.t
-     and type Entity_type.t = Entity_type.t
-     and type Entity_type.Set.t = Entity_type.Set.t
-     and type 'a Entity_type.Map.t = 'a Entity_type.Map.t
-     and type Entity.t = Entity.t
-     and type Entity.Set.t = Entity.Set.t
-     and type 'a Entity.Map.t = 'a Entity.Map.t
+    with module Attribute_type = Attribute_type
+     and module Attribute_uniqueness = Attribute_uniqueness
+     and module Relation = Relation
+     and module Entity_type = Entity_type
+     and module Entity = Entity
 
   val transaction : ((module T) -> unit Lwt.t) -> unit Lwt.t
 end
