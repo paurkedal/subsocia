d73be888fc75667252090a6d54ed99fb9131ff52
Petter A. Urkedal <paurkedal@gmail.com>
2025-11-16 20:30:11 +0100

    Test Entity.super and Entity.sub.

--- tests/test_inclusion.ml
+++ tests/test_inclusion.ml
@@ -1,4 +1,4 @@
-(* Copyright (C) 2015--2023  Petter A. Urkedal <paurkedal@gmail.com>
+(* Copyright (C) 2015--2025  Petter A. Urkedal <paurkedal@gmail.com>
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -45,6 +45,7 @@ module Perf = struct
   let show () =
     let keys = Hashtbl.fold (fun s _ -> String_set.add s) ht String_set.empty in
     let len = String_set.fold (max % String.length) keys 0 in
+    printf "%*s  %6s %8s %8s %8s %8s\n" len "" "n" "tC" "tW" "tC/n" "tW/n";
     keys |> String_set.iter @@ fun tag ->
       let tC, tW, n = get tag in
       if n = 0 then
@@ -59,6 +60,10 @@ let test n =
   let%lwt root = Entity.get_root () in
   let ea = Array.make n root in
   let ia = Array.init n (fun _ -> Array.make n false) in
+  let pp_entity =
+    let get_index e = Option.get (Array.find_index (Entity.equal e) ea) in
+    Fmt.(using get_index (any "x" ++ int))
+  in
   let%lwt org_group = Entity_type.of_name_exn "org_group" in
   Perf.start "insert";
   for%lwt i = 0 to n - 1 do
@@ -99,20 +104,50 @@ let test n =
     done
   end >>= fun () ->
   Perf.stop_lwt "closure" >>= fun () ->
+  for i = 0 to n - 1 do
+    printf "%2d " i;
+    for j = 0 to n - 1 do
+      if j mod 10 = 0 then print_char '|';
+      if ia.(i).(j) then printf "%d" (j mod 10) else print_char ' ';
+    done;
+    print_char '\n'
+  done;
   Perf.start_lwt "is_sub" >>= fun () ->
   for%lwt i = 0 to n - 1 do
     for%lwt j = 0 to i - 1 do
       Perf.step ~dn:2 "is_sub";
       let%lwt issub = Entity.is_sub ea.(i) ea.(j) in
       let%lwt issup = Entity.is_sub ea.(j) ea.(i) in
-      let%lwt id_i = Entity.soid ea.(i) in
-      let%lwt id_j = Entity.soid ea.(j) in
-      Alcotest.(check bool) (Fmt.str "#%ld ⊆ #%ld" id_i id_j) ia.(i).(j) issub;
-      Alcotest.(check bool) (Fmt.str "#%ld ⊇ #%ld" id_i id_j) false issup;
+      Alcotest.(check bool) (Fmt.str "e%d ⊆ e%d" i j) ia.(i).(j) issub;
+      Alcotest.(check bool) (Fmt.str "e%d ⊇ e%d" i j) false issup;
       Lwt.return_unit
     done
   done >>= fun () ->
   Perf.stop_lwt "is_sub" >>= fun () ->
+  Perf.start_lwt "super" >>= fun () ->
+  for%lwt i = 0 to n - 1 do
+    let%lwt super = Entity.super ea.(i) in
+    Fmt.pr "super x%d = {@[%a@]}@." i Fmt.(list ~sep:comma pp_entity)
+      (Entity.Set.elements super);
+    for%lwt j = 0 to n - 1 do
+      Alcotest.(check bool) (Fmt.str "x%d ∈ super x%d" j i) ia.(i).(j)
+        (Entity.Set.mem ea.(j) super);
+      Lwt.return_unit
+    done
+  done >>= fun () ->
+  Perf.stop_lwt "super" >>= fun () ->
+  Perf.start_lwt "sub" >>= fun () ->
+  for%lwt i = 0 to n - 1 do
+    let%lwt sub = Entity.sub ea.(i) in
+    Fmt.pr "sub x%d = {@[%a@]}@." i Fmt.(list ~sep:comma pp_entity)
+      (Entity.Set.elements sub);
+    for%lwt j = 0 to n - 1 do
+      Alcotest.(check bool) (Fmt.str "x%d ∈ sub x%d" j i) ia.(j).(i)
+        (Entity.Set.mem ea.(j) sub);
+      Lwt.return_unit
+    done
+  done >>= fun () ->
+  Perf.stop_lwt "sub" >>= fun () ->
   Perf.start_lwt "delete" >>= fun () ->
   for%lwt i = n - 1 downto 0 do
     Perf.step "delete";
